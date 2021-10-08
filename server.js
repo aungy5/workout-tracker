d25c47ee09ebadd80759624bbf3723144370d8e8
Andrew Ungvary <aungvary5@gmail.com>
2021-10-07 22:32:43 -0400

    adding server.js file and models folder

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const logger = require("morgan");
+
+const PORT = process.env.port || 3001;
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
+  useNewUrlParser: true,
+  //useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
